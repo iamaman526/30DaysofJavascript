3ae9fd6c12d0c4055f406c1674b21d9cef069d72
iamaman526 <iamaman526@gmail.com>
2023-02-06 17:22:02 +0530

    day02

---
+++ Day1/Console.js
@@ -0,0 +1,27 @@
+console.log('Hello', 'World', '!')
+
+console.log('hello', 'aman', 2023)
+
+console.log('Welcome', 'to', 30, 'Days', 'Of', 'JavaScript')
+
+//console.log can pass multiple value 
+// it is a a built-in function 
+// we pass the argument as a input data and function displays Output
+
+/*
+This is a multiline comment  
+ Multiline comments can take multiple lines  
+ JavaScript is the language of the web  
+ */
+
+
+
+//Arthemtics inside the console
+
+console.log(2 + 3);  //addition
+console.log(3 -2);   //Subtraction
+console.log(3/3);     // divison
+console.log(3*3);     //Multiplication
+console.log(3%2);    //Mdulus - finding remainder
+console.log(3**2);  //exponential i.e 3**3 === 3*3
+
