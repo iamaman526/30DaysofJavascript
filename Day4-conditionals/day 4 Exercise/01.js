d42974dc7b9910bc56b2119d49608e9cf629cd80
iamaman526 <iamaman526@gmail.com>
2023-03-12 23:42:01 +0530

    day4-question1-done

--- Day4-conditionals/day 4 Exercise/01.js
+++ Day4-conditionals/day 4 Exercise/01.js
@@ -1,4 +1,4 @@
-// Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+//1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
 const year = new Date();
 let getyear = year.getFullYear();
@@ -25,3 +25,20 @@ if (yearborn.toString().length !== 4) {
     );
   }
 }
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
+
+let yourAge = parseInt(prompt("enter your age"));
+
+let myage =  25;
+let agediff = myage - yourAge ;
+
+if (myage > yourAge) {
+  console.log(`i am ${agediff} years older than you`);
+  
+}
+else(myage < yourAge)
+  console.log(`you are ${agediff} years older than me`)
+
+  
