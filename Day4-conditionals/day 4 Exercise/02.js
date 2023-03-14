68fdf45125ae58ad13a6835ff5c462c1302efc80
iamaman526 <iamaman526@gmail.com>
2023-03-14 23:20:10 +0530

    day4-ques2

--- Day4-conditionals/day 4 Exercise/02.js
+++ Day4-conditionals/day 4 Exercise/02.js
@@ -10,18 +10,37 @@ if (score < 0 || score > 100) {
   alert("enter valid score");
 } else if (score >= 80 && score <= 100) {
   console.log("you are graded 'A'");
+} else if (score >= 70 && score <= 79) {
+  console.log("you are graded 'B'");
+} else if (score >= 60 && score <= 69) {
+  console.log("you are graded 'C'");
+} else if (score >= 50 && score <= 59) {
+  console.log("you are graded 'D'");
+} else if (score <= 49) {
+  console.log("you are graded 'F'");
 }
 
-  else if (score >= 70 && score <= 79) {
-    console.log("you are graded 'B'");
-  }
-  else if (score >= 60 && score <= 69) {
-    console.log("you are graded 'C'");
-  }
-  else if (score >= 50 && score <= 59) {
-    console.log("you are graded 'D'");
-  }
-else if(score <=49 ){
-    console.log("you are graded 'F'");
+// Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+let month = String(prompt("enter the month to know the season "));
+month = month.charAt(0).toUpperCase() + month.slice(1).toLowerCase();
+if (month === "September" || month === "October" || month === "November") {
+    console.log(`the season is Autumn in ${month}`);
 }
-  
+
+else if (month === "December" || month === "January" || month === "February" ){
+  console.log(`the season is Winter in ${month}`);
+}
+else if (month === "March" || month === "April" || month === "May" ){
+  console.log(`the season is Spring in ${month}`);
+}
+else if (month === "June" || month === "July" || month === "August" ){
+  console.log(`the season is Summer in ${month}`);
+}
+else{
+  console.log("enter the correct month");
+}
