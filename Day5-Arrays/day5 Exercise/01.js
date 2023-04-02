9e77390645602ef89b881cfe31a28e216fadc3ff
iamaman526 <iamaman526@gmail.com>
2023-04-02 20:12:25 +0530

    exercise5

---
+++ Day5-Arrays/day5 Exercise/01.js
@@ -0,0 +1,10 @@
+// 1.Declare an empty array;
+
+const arr = Array();
+const arrr = [];
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// 2.Declare an array with more than 5 number of elements
+
+const arr5 = ["a","d","e","g", "f"]
+console.log(arr5);
