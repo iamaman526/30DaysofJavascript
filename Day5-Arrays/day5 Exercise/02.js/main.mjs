30a413b6f4481685742213010a639ac02c7814aa
iamaman526 <iamaman526@gmail.com>
2023-05-03 18:27:24 +0530

    completed-exercise-2

--- Day5-Arrays/day5 Exercise/02.js/main.mjs
+++ Day5-Arrays/day5 Exercise/02.js/main.mjs
@@ -16,8 +16,59 @@ console.log(text);
 console.log(removepunc);
 // Split the sentence into an array of words
 
-const sentenceinarray = removepunc.split(" ") //using the space character
+const sentenceinarray = removepunc.split(" "); //using the space character
 console.log(sentenceinarray);
 
- const lengthofsentence = sentenceinarray.length;//lenth of words
- console.log(lengthofsentence);
+const lengthofsentence = sentenceinarray.length; //lenth of words
+console.log(lengthofsentence);
+
+// 3. In the following shopping cart add, remove, edit items
+
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+
+shoppingCart.unshift("meat");
+console.log(shoppingCart);
+
+// add Sugar at the end of you shopping cart if it has not been already added.
+
+shoppingCart.push("sugar");
+console.log(shoppingCart);
+
+//  remove 'Honey' if you are allergic to honey
+console.log(shoppingCart);
+shoppingCart.splice(4, 1);
+console.log(shoppingCart); //honey removed
+
+//   modify Tea to 'Green Tea'
+shoppingCart.splice(3, 1, "greenTea");
+console.log(shoppingCart);
+
+//4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+if (countries.includes("ETHIOPIA")) {
+  console.log("ETHIOPIA");
+} else {
+  countries.push("ETHIOPIA");
+}
+
+console.log(countries);
+
+// 5. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+if (webTechs.includes("Sass")) {
+  console.log("Sass is a CSS preprocess");
+}
+else{
+    webTechs.push("Sass")
+}
+ console.log(webTechs);
+
+//6. Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
