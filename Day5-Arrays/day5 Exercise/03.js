cb651645a8af23c3d4c01be8a700e704947e6373
iamaman526 <iamaman526@gmail.com>
2023-05-03 18:58:34 +0530

    updatedexercise3-question1

---
+++ Day5-Arrays/day5 Exercise/03.js
@@ -0,0 +1,9 @@
+// 1.The following is an array of 10 students ages:
+
+// Sort the array and find the min and max age
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+ages.sort() //sort the array in ascending order 
+console.log(ages);
+console.log(ages[0]); // after sorting index will give the min value 
+console.log(ages[ages.length-1]); // after sorting last value will give max value
