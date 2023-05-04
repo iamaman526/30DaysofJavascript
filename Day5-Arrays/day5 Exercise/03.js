6147565fdbe4ca3a6c2f1c2513665af29cf1ea8c
iamaman526 <iamaman526@gmail.com>
2023-05-04 19:12:04 +0530

    exercise3-updated

--- Day5-Arrays/day5 Exercise/03.js
+++ Day5-Arrays/day5 Exercise/03.js
@@ -3,7 +3,36 @@
 // Sort the array and find the min and max age
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
-ages.sort() //sort the array in ascending order 
+ages.sort(); //sort the array in ascending order
 console.log(ages);
-console.log(ages[0]); // after sorting index will give the min value 
-console.log(ages[ages.length-1]); // after sorting last value will give max value
+console.log(ages[0]); // after sorting index will give the min value
+console.log(ages[ages.length - 1]); // after sorting last value will give max value
+
+//Find the median age(one middle item or two middle items divided by two)
+console.log(ages); //sorted
+
+const middle1 = ages[ages.length / 2 - 1];
+const middle2 = ages[ages.length / 2];
+const median = (middle1 + middle2) / 2;
+console.log(median);
+
+// Find the average age(all items divided by number of items)
+
+let sum = 0;
+
+for (let i = 0; i < ages.length; i++) {
+  sum += ages[i];
+}
+console.log(sum);
+
+const average = sum / ages.length;
+console.log(average);
+
+// const sum = ages.reduce((total, age) => total + age, 0);
+// const average = sum / ages.length;  // another method by using accumalator in reduce method
+
+
+//Find the range of the ages(max minus min)
+
+const range = [ages.length-1] - ages[0]
+console.log(range);
